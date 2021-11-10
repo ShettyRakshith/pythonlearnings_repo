1e823759d95740b49be50509241645c027af6b93
ShettyRakshith <rakshithshettyb00@gmail.com>
2021-11-10 07:10:22 +0530

    Added the age critiria check logic in CollectPersonalDetails module

--- CollectPersonalDetail.py
+++ CollectPersonalDetail.py
@@ -3,3 +3,9 @@
 print("--Good Day-- this is the sample program")
 name=input("What is your good name")
 age=input("Please state your age")
+
+#check if major in age
+if age<18:
+ print("please add the parent details as well")
+ parent_name=input("what is your parent name")
+ 
